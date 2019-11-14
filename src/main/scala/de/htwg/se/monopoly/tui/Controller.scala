9bcf7b0ca493504297e13d8fa2fd473d71332235
Alexander-Robert Keller <keller.ar98@gmail.com>
2019-11-14 15:34:19 +0100

    Updated: tui

--- src/main/scala/de/htwg/se/monopoly/tui/Controller.scala
+++ src/main/scala/de/htwg/se/monopoly/tui/Controller.scala
@@ -1,5 +1,9 @@
 package de.htwg.se.monopoly.tui
 
 class Controller {
+  val mainMenue = "option | description\n [1]   | Start new game\n [2]   | Exit game"
 
+  val gameMenue = "option | description\n [1]   | roll dice\n [2]   | Exit game"
+
+  val jailMenue = "option | description\n [1]   | roll dice\n [2]   | Exit game"
 }
--- src/main/scala/de/htwg/se/monopoly/tui/TextualUserInterface.scala
+++ src/main/scala/de/htwg/se/monopoly/tui/TextualUserInterface.scala
@@ -1,12 +1,14 @@
 package de.htwg.se.monopoly.tui
-
+import de.htwg.se.monopoly.tui.Controller
 /**
  * TODO Come up with a suitable observer pattern for the textual user interface
  */
 class TextualUserInterface {
+  val controller = new Controller
+
   def runMenuPrompt(): Unit = {
-    println("[1] Start New Game")
-    println("[2] Start Exit Game")
+
+    println(controller.mainMenue)
     scala.io.StdIn.readInt()
   }
 }
