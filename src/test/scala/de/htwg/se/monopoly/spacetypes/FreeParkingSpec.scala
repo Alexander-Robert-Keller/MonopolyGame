4b9003ca184e05e40e2ff3ec0bfe21b29897ae71
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2019-11-14 15:05:26 +0100

    Updated: Freeparking + Go (Spec)

--- src/test/scala/de/htwg/se/monopoly/spacetypes/FreeParkingSpec.scala
+++ src/test/scala/de/htwg/se/monopoly/spacetypes/FreeParkingSpec.scala
@@ -3,22 +3,22 @@ package de.htwg.se.monopoly.spacetypes
 import de.htwg.se.monopoly.Player
 import org.scalatest.{Matchers, WordSpec}
 
-class CommunityChestSpec extends WordSpec with Matchers{
+class FreeParkingSpec extends WordSpec with Matchers{
 
-  "a CommunityChest spacetype" when {
+  "a FreeParking spacetype" when {
     "new" should {
-      val communityChest = CommunityChest()
+      val freeParking = FreeParking()
       val player = Player(0)
       "have a way to tell if the player is on the space" in {
-        communityChest.isOnSpace(player) should be (false)
+        freeParking.isOnSpace(player) should be (false)
       }
       "have a way to add a player, if he gets onto the space" in {
-        communityChest.addPlayer(player)
-        communityChest.isOnSpace(player) should be (true)
+        freeParking.addPlayer(player)
+        freeParking.isOnSpace(player) should be (true)
       }
       "have a way to remove the player if he leaves the space" in {
-        communityChest.removePlayer(player)
-        communityChest.isOnSpace(player) should be (false)
+        freeParking.removePlayer(player)
+        freeParking.isOnSpace(player) should be (false)
       }
       "have a way to trigger a specific action if needed" in {
         //not jet implemented
