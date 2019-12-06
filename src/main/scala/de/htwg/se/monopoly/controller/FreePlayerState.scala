c1611832472acf47287199d524b463a5f86b5a35
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2019-12-06 10:23:11 +0100

    added State pattern in controller to differentiate between jailed and free player

---
+++ src/main/scala/de/htwg/se/monopoly/controller/FreePlayerState.scala
@@ -0,0 +1,40 @@
+package de.htwg.se.monopoly.controller
+
+import de.htwg.se.monopoly.Game
+import de.htwg.se.monopoly.model.{Dice, Player}
+
+object FreePlayerState extends PlayerState {
+  override def determinePlayerState(currentPlayer: Player): Unit = {
+    if (currentPlayer.isJailed) {
+      playerState = JailedPlayerState
+    } else {
+      playerState = FreePlayerState
+    }
+  }
+
+  override def rollDice(dice: Dice): String = {
+    if (Game.board.getDice.gotDoublets()) {
+      if (dice.getEyes + Game.board.players(GameState.currentPlayer).getLocation == Game.board.totalNumberOfSpaces) {
+        Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+        return dice.toString + "You landed on Go: here are 400$\n"
+      }
+      if (dice.getEyes + Game.board.players(GameState.currentPlayer).getLocation > Game.board.totalNumberOfSpaces) {
+        Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+        return dice.toString + "You went over Go: here are 200$\n"
+      }
+      Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+      dice.toString
+    } else {
+      if (dice.getEyes + Game.board.players(GameState.currentPlayer).getLocation == Game.board.totalNumberOfSpaces) {
+        Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+        return dice.toString + "You went over Go: here are 400$\n"
+      }
+      if (dice.getEyes + Game.board.players(GameState.currentPlayer).getLocation > Game.board.totalNumberOfSpaces) {
+        Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+        return dice.toString + "You went over Go: here are 200$\n"
+      }
+      Game.board.movePlayer(dice.getEyes, GameState.currentPlayer)
+      dice.toString
+    }
+  }
+}
