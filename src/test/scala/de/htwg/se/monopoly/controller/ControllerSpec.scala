35e87c276aaa4864f1609faad78a6a28e4b1aa0f
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2019-12-06 02:14:56 +0100

    updated ControllerSpec

--- src/test/scala/de/htwg/se/monopoly/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/monopoly/controller/ControllerSpec.scala
@@ -1,8 +1,8 @@
 package de.htwg.se.monopoly.controller
 
 import org.scalatest.{Matchers, WordSpec}
-
 import de.htwg.se.monopoly.Game
+import de.htwg.se.monopoly.model.Player
 
 class ControllerSpec extends WordSpec with Matchers{
   "A Controller" when {
@@ -24,14 +24,44 @@ class ControllerSpec extends WordSpec with Matchers{
         Game.init()
         controller.stringGameBoard() should be (Game.board.toString())
       }
-      "have a method that initializes the game" in {
-        controller.initializeGame()
-        Game.isRunning should be (true)
+      "have a way to roll a dice for a player and respond with certain Strings depending on the outcome" in {
+        // roll dice test from start
+        GameState.setCurrentPlayer(0)
+        Game.board.players(0) = Player(Game.board.players(0).getId, 0, jailed = false, Game.board.players(0).getMoney)
+        controller.rollDie()
+        if (Game.board.dice.gotDoublets()) {
+          GameState.getCurrentPlayer should be (0)
+        } else {
+          GameState.getCurrentPlayer should be (1)
+        }
+        // roll dice test over start
+        GameState.setCurrentPlayer(0)
+        Game.board.players(0) = Player(Game.board.players(0).getId, Game.board.totalNumberOfSpaces - 1, jailed = false,
+          Game.board.players(0).getMoney)
+        controller.rollDie()
+        if (Game.board.dice.gotDoublets()) {
+          GameState.getCurrentPlayer should be (0)
+        } else {
+          GameState.getCurrentPlayer should be (1)
+        }
+        // roll dice test hit exact go position
+        GameState.setCurrentPlayer(0)
+        Game.board.players(0) = Player(Game.board.players(0).getId, Game.board.totalNumberOfSpaces - 6, jailed = false,
+          Game.board.players(0).getMoney)
+        controller.rollDie()
+        if (Game.board.players(0).getLocation == 0) {
+          if (Game.board.dice.gotDoublets()) {
+            GameState.getCurrentPlayer should be(0)
+          } else {
+            GameState.getCurrentPlayer should be(1)
+          }
+        }
       }
       "have a method that exits the current game" in {
+        Game.setRunning(true)
         controller.exitCurrentGame() should be (controller.exitCurrentGameMessage)
         Game.isRunning should be (false)
-        //TODO: Game.currentGameState should be ("MainMenu")
+        GameState.currentGameStateMessage should be ("Main Menu:")
       }
       "have a String that contains a message when u exit a Game" in {
         controller.exitCurrentGameMessage should be ("Returns to main menu!")
@@ -39,10 +69,15 @@ class ControllerSpec extends WordSpec with Matchers{
       "have a String that contains a message when u exit the program" in {
         controller.exitProgramMessage should be ("Exit game!")
       }
-      "have a way to notify his Observers" in {
-        //TODO: Game.currentPlayer = 0
-        controller.notifyObservers()
-        //TODO: Game.currentPlayer = 1
+      "have a method that returns a String which tells us the next player" in {
+        GameState.setCurrentPlayer(0)
+        controller.nextPlayersRoundMessage() should be ("It´s Player 1´s turn!\n")
+      }
+      "have a method that initializes the game" in {
+        GameState.setState("MAIN_MENU")
+        controller.initializeGame()
+        Game.isRunning should be (true)
+        GameState.state should be (GameState.ROLL_DICE)
       }
     }
   }
