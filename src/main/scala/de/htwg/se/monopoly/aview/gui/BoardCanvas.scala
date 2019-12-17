67f1cd720d875580598b970a61f10d13d1b7f85d
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2019-12-17 16:17:41 +0100

    updated Board Canvas

--- src/main/scala/de/htwg/se/monopoly/aview/gui/BoardCanvas.scala
+++ src/main/scala/de/htwg/se/monopoly/aview/gui/BoardCanvas.scala
@@ -2,28 +2,75 @@ package de.htwg.se.monopoly.aview.gui
 
 import java.awt.{Dimension, Image}
 import java.io.File
+
+import de.htwg.se.monopoly
+import de.htwg.se.monopoly.Game
+import de.htwg.se.monopoly.controller.GameState
+import de.htwg.se.monopoly.model.Player
 import javax.imageio.ImageIO
 
 import scala.swing.{Component, Graphics2D}
 
 class BoardCanvas extends Component{
 
-  preferredSize = new Dimension(900, 900)
+  preferredSize = new Dimension(1000, 1000)
 
   override def paintComponent(g: Graphics2D): Unit = {
-    preferredSize = new Dimension(900, 900)
+    preferredSize = new Dimension(1000, 1000)
     val gameBoardImage = getMonopolyBoardImage
     g.drawImage(gameBoardImage, 0, 0, null)
-    //TODO: add algorithm that draws at certain positions players -> maybe with an Image
-    g.drawLine(100,0,100, 900)
-    g.drawLine(0,233,100, 900)
-    g.drawLine(200,231,100, 900)
+
+    val numberofPlayers = GameState.getNumberOfPlayers
+    var currentPlayer = 0
+    var yOffSet = 0
+    //TODO: adjust prefeerd size back to 900 x 900 and tweak x and y calculation
+    do {
+      val location = Game.board.players(currentPlayer).getLocation  //TODO: implement in controller getplayers -> returns list
+      val playerImage = getPlayerImage(currentPlayer, numberofPlayers)
+      if (location < 10) {
+        val x = 925 - (location * 100)
+        val y = 900 + yOffSet
+        g.drawImage(playerImage, x, y, null)
+        currentPlayer += 1
+        yOffSet += 10
+      } else if (location < 20) {
+        val x = 25
+        val y = 900 - ((location - 10) * 100) + yOffSet
+        g.drawImage(playerImage, x, y, null)
+        currentPlayer += 1
+        yOffSet += 10
+      } else if (location < 30) {
+        val x = 25 + ((location - 20) * 100)
+        val y = 25 + yOffSet
+        g.drawImage(playerImage, x, y, null)
+        currentPlayer += 1
+        yOffSet += 10
+      } else if (location < 40) {
+        val x = 925
+        val y = 25  + ((location - 30) * 100) + yOffSet
+        g.drawImage(playerImage, x, y, null)
+        currentPlayer += 1
+        yOffSet += 10
+      }
+    } while (currentPlayer <  numberofPlayers)
+  }
+
+  def getPlayerImage(currentPlayer: Int, totalNumberOfPlayers: Int): Image = {
+   /* if (currentPlayer > totalNumberOfPlayers) {
+      val path = "src/main/scala/de/htwg/se/monopoly/aview/gui/images/PlayerImage" + currentPlayer.toString + ".jpg"
+    } else {
+      //Default pic
+    }*/
+    val path = "src/main/scala/de/htwg/se/monopoly/aview/gui/images/MonopolyMascot.jpg"
+    val gameBoardImage = ImageIO.read(new File(path))
+    val resized = gameBoardImage.getScaledInstance(50, 50, Image.SCALE_DEFAULT)
+    resized
   }
 
   def getMonopolyBoardImage: Image = {
     val path = "src/main/scala/de/htwg/se/monopoly/aview/gui/images/MonopolygameBoard.jpg"
     val gameBoardImage = ImageIO.read(new File(path))
-    val resized = gameBoardImage.getScaledInstance(900, 900, Image.SCALE_DEFAULT)
+    val resized = gameBoardImage.getScaledInstance(1000, 1000, Image.SCALE_DEFAULT)
     resized
   }
 }
