7968c1f13d4694d7a6f8a7a49bfc2f11eee20adc
Daniel Ozyurt <danielozyurt@icloud.com>
2020-01-16 10:53:15 +0100

    edit PlayerState.scala

--- src/main/scala/de/htwg/se/monopoly/model/Player.scala
+++ src/main/scala/de/htwg/se/monopoly/model/Player.scala
@@ -1,22 +1,22 @@
 package de.htwg.se.monopoly.model
 
-case class Player(t_id: Int, location: Int, jailed: Boolean, money: Int) {
+case class Player(id: Int, location: Int, jailed: Boolean, money: Int) {
 
   def getLocation: Int = location
 
-  def move(spaces: Int, totalNumberOfSpaces: Int): Player = Player(t_id, (location + spaces) % totalNumberOfSpaces, jailed, money)
+  def move(spaces: Int, totalNumberOfSpaces: Int): Player = Player(id, (location + spaces) % totalNumberOfSpaces, jailed, money)
 
   def isJailed: Boolean = jailed
 
-  def setJailed(jail: Boolean, position: Int): Player = Player(t_id, position, jailed, money)
+  def setJailed(jail: Boolean, position: Int): Player = Player(id, position, jailed, money)
 
   def getMoney: Int = money
 
-  def increaseMoney(amount: Int): Player = Player(t_id, location, jailed, money + amount)
+  def increaseMoney(amount: Int): Player = Player(id, location, jailed, money + amount)
 
-  def decreaseMoney(amount: Int): Player = Player(t_id, location, jailed, money - amount)
+  def decreaseMoney(amount: Int): Player = Player(id, location, jailed, money - amount)
 
   override def toString: String = "Player " + getId
 
-  def getId: Int = t_id
+  def getId: Int = id
 }
