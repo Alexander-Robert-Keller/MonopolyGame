8bcb36e0b4eab3d7c2ed825166b38cf398502bb8
Daniel Ozyurt <danielozyurt@icloud.com>
2020-01-16 10:53:15 +0100

    edit PlayerState.scala

--- src/main/scala/de/htwg/se/monopoly/model/spacetypes/Space.scala
+++ src/main/scala/de/htwg/se/monopoly/model/spacetypes/Space.scala
@@ -3,5 +3,5 @@ package de.htwg.se.monopoly.model.spacetypes
 import de.htwg.se.monopoly.model.Player
 
 trait Space {
-  def action(t_player: Player): Player
+  def action(player: Player): Player
 }
