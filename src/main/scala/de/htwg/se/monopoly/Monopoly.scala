5c6577a7a3e1f707222b6106e86d68b4a10e4cb8
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2020-01-17 10:02:45 +0100

    test

--- src/main/scala/de/htwg/se/monopoly/Monopoly.scala
+++ src/main/scala/de/htwg/se/monopoly/Monopoly.scala
@@ -1,12 +1,17 @@
+package de.htwg.se.monopoly
+
+import com.google.inject.Guice
 import de.htwg.se.monopoly.aview.guiComponent.guiBaseImpl.GUI
 import de.htwg.se.monopoly.aview.tuiComponent.tuiBaseImpl.TextualUserInterface
+import de.htwg.se.monopoly.controller.controllerComponent.ControllerInterface
 import de.htwg.se.monopoly.controller.controllerComponent.controllerBaseImpl.Controller
 
 import scala.io.StdIn
 
-object Main {
+object Monopoly {
   def main(args: Array[String]): Unit = {
-    val controller: Controller = new Controller
+    val injector = Guice.createInjector(new MonopolyModule)
+    val controller: Controller = injector.getInstance(classOf[ControllerInterface])
     val tui: TextualUserInterface = new TextualUserInterface(controller)
     val gui: GUI = new GUI(controller)
 
--- src/test/scala/MonopolySpec.scala
+++ src/test/scala/MonopolySpec.scala
@@ -1,10 +1,11 @@
+import de.htwg.se.monopoly.Monopoly
 import org.scalatest.{Matchers, WordSpec}
 
-class MainSpec extends WordSpec with Matchers {
+class MonopolySpec extends WordSpec with Matchers {
 
   "The Monopoly main class" should {
     "accept text input as argument without readline loop, to test commands" in {
-      Main.main(Array[String]("1", "2"))
+      Monopoly.main(Array[String]("1", "2"))
     }
   }
 }
