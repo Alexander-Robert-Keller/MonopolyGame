f4fee68591891e0261f53f99335bbcc6d24de808
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2020-01-23 12:32:11 +0100

    fixed tests

--- src/test/scala/de/htwg/se/monopoly/aview/tuiComponent/TUI_MenuSpec.scala
+++ src/test/scala/de/htwg/se/monopoly/aview/tuiComponent/TUI_MenuSpec.scala
@@ -12,17 +12,6 @@ class TUI_MenuSpec extends WordSpec with Matchers {
       val injector = Guice.createInjector(new MonopolyModule)
       val controller = injector.getInstance(classOf[ControllerInterface])
       controller.initializeGame()
-      "have a method determine playerState" in {
-        MainMenu.determineMenu(GameState(0, 0, 2)) should equal (MainMenu)
-        MainMenu.determineMenu(GameState(1, 0, 2)) should equal (RollDiceMenu)
-        MainMenu.determineMenu(GameState(2, 0, 2)) should equal (BuyMenu)
-        MainMenu.determineMenu(GameState(3, 0, 2)) should equal (FinishedGameMenu)
-        try {
-          MainMenu.determineMenu(GameState(4, 0, 2))
-        } catch {
-          case e: IllegalArgumentException => e.getMessage should be ("there is no such state")
-        }
-      }
       "have a method toString" in {
         MainMenu.toString shouldBe a[String]
       }
@@ -34,14 +23,38 @@ class TUI_MenuSpec extends WordSpec with Matchers {
       "have a MainMenu" in {
         MainMenu.menuOptions.size should not be 0
       }
+      "have for MainMenu a method determine playerState" in {
+        MainMenu.determineMenu(GameState(0, 0, 2)) should equal (MainMenu)
+        MainMenu.determineMenu(GameState(1, 0, 2)) should equal (RollDiceMenu)
+        MainMenu.determineMenu(GameState(2, 0, 2)) should equal (BuyMenu)
+        MainMenu.determineMenu(GameState(3, 0, 2)) should equal (FinishedGameMenu)
+      }
       "have a BuyMenu" in {
         BuyMenu.menuOptions.size should not be 0
+      }
+      "have for BuyMenu a method determine playerState" in {
+        BuyMenu.determineMenu(GameState(0, 0, 2)) should equal (MainMenu)
+        BuyMenu.determineMenu(GameState(1, 0, 2)) should equal (RollDiceMenu)
+        BuyMenu.determineMenu(GameState(2, 0, 2)) should equal (BuyMenu)
+        BuyMenu.determineMenu(GameState(3, 0, 2)) should equal (FinishedGameMenu)
       }
       "have a RollDiceMenu" in {
         RollDiceMenu.menuOptions.size should not be 0
       }
+      "have for RollDiceMenu a method determine playerState" in {
+        RollDiceMenu.determineMenu(GameState(0, 0, 2)) should equal (MainMenu)
+        RollDiceMenu.determineMenu(GameState(1, 0, 2)) should equal (RollDiceMenu)
+        RollDiceMenu.determineMenu(GameState(2, 0, 2)) should equal (BuyMenu)
+        RollDiceMenu.determineMenu(GameState(3, 0, 2)) should equal (FinishedGameMenu)
+      }
       "have a FinishedGameMenu" in {
         FinishedGameMenu.menuOptions.size should not be 0
+      }
+      "have for FinishedGameMenu a method determine playerState" in {
+        FinishedGameMenu.determineMenu(GameState(0, 0, 2)) should equal (MainMenu)
+        FinishedGameMenu.determineMenu(GameState(1, 0, 2)) should equal (RollDiceMenu)
+        FinishedGameMenu.determineMenu(GameState(2, 0, 2)) should equal (BuyMenu)
+        FinishedGameMenu.determineMenu(GameState(3, 0, 2)) should equal (FinishedGameMenu)
       }
     }
   }
